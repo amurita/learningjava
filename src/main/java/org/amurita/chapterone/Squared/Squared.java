29ef4c5e92576cd065be1771d2a9f432f72b05f7
Amurita <amurita.dev@gmail.com>
2018-02-16 11:23:47 +0100

    Added first example (squared).

---
+++ src/main/java/org/amurita/chapterone/Squared/Squared.java
@@ -0,0 +1,27 @@
+/**
+ * First Java example program.
+ * @version 1.0 15th of February, 2018
+ * @author Amurita
+ */
+
+package org.amurita.chapterone.Squared;
+
+// Class to square different Integer values.
+public class Squared {
+
+    // Build the square of given Integer value.
+    static int squared(int n) {
+        return n * n;
+    }
+
+    // Output of the squared Integer values from 1 - 4.
+    static void output(int n) {
+        for (int i = 1; i <= n; i = i + 1) {
+            String s = "Squared(" + i + ") = " + squared(i);
+            System.out.println(s);
+        }
+    }
+    public static void main( String[] args ) {
+        output( 4 );
+    }
+}
