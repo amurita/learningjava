2e8129b8763e3266387b36bfcd0f2e82fbcfe867
Amurita <amurita.dev@gmail.com>
2018-02-18 17:24:10 +0100

    Added class to show how to declare your first variables.

---
+++ src/main/java/org/amurita/chaptertwo/FirstVariable.java
@@ -0,0 +1,21 @@
+/**
+ * Fifth Java example program.
+ * @version 1.0 17th of February, 2018
+ * @author Amurita
+ */
+
+package org.amurita.chaptertwo;
+
+// Class to declare differnt variables.
+public class FirstVariable {
+
+    // Main method to set differnt variables.
+    public static void main( String[] args ) {
+        String name; // Name
+        int age; // Age of the person
+        double income; // Income of the person
+        char gender; // Gender of the person ('f' oder 'm')
+        boolean isPresident; // Is the person a president? (true or false)
+        boolean isVegetarian; // Is the person a vegetarian?
+    }
+}
