50364f43b28b4ab829783a91b062a73bc5bb3250
Amurita <amurita.dev@gmail.com>
2018-02-18 12:06:29 +0100

    Refactored Squared in chapter one and Added two new classes to chapter two (OverloadedPrintln and VarArgs).

---
+++ src/main/java/org/amurita/chaptertwo/VarArgs.java
@@ -0,0 +1,17 @@
+/**
+ * Thirs Java example program.
+ * @version 1.0 16th of February, 2018
+ * @author Amurita
+ */
+
+package org.amurita.chaptertwo;
+
+// Class to visualize variable argument lists.
+public class VarArgs {
+
+    // Show how variable argument lists works.
+    public static void main( String[] args ) {
+        System.out.printf( "Was sagst du?%n" ); // Added newline at the end.
+        System.out.printf( "%d Kanäle und überall nur %s.%n", 220, "Katzen" ); // Usage of variable argument list.
+    }
+}
