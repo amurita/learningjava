2c78a0708a8788ac8edca4f241c2dcde4ebb54d7
Amurita <amurita.dev@gmail.com>
2018-02-18 12:25:22 +0100

    Added SystemOutErr (ex. for StandardError-/Standard-Output).

---
+++ src/main/java/org/amurita/chaptertwo/SystemOutErr.java
@@ -0,0 +1,17 @@
+/**
+ * Fourth Java example program.
+ * @version 1.0 16th of February, 2018
+ * @author Amurita
+ */
+
+package org.amurita.chaptertwo;
+
+// Class to show how to print to std.-output/-error.
+public class SystemOutErr {
+
+    // Print std output and - error messages.
+    public static void main( String[] args ) {
+        System.out.println( "This line uses standard output." ); // Standard output.
+        System.err.println( "This line uses standard error output." ); // Standard Error output.
+    }
+}
