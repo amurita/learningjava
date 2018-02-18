da6df38d202012d3e21d7a4feb2e9b6538191ef7
Amurita <amurita.dev@gmail.com>
2018-02-18 17:40:49 +0100

    Added class to show variable declaration (VariableDeclaration).

---
+++ src/main/java/org/amurita/chaptertwo/VariableDeclaration.java
@@ -0,0 +1,20 @@
+/**
+ * Sixth Java example program.
+ * @version 1.0 17th of February, 2018
+ * @author Amurita
+ */
+
+package org.amurita.chaptertwo;
+
+// Class to show some variable declarations.
+public class VariableDeclaration {
+
+    // Declare some variables.
+    public static void main( String[] args ) {
+        String name = "Barack Hussein Obama II";
+        int age = 48;
+        double income = 400000;
+        char gender = 'm';
+        boolean isPresident = false;
+    }
+}
